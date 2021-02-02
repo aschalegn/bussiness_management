c2c4efad9d0d7df8e9488db2342b5872ad2507aa
aschaley ascheneke <schaley1@gmail.com>
2021-02-02 11:49:07 +0200

    config and server

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//* DB Connection
+const dbUrl = "mongo://localhost:27017/bussiness";
+mongoose.connect(dbUrl, { useFindAndModify: true })
+    .then(con => {
+        console.log("connected to db");
+    }).catch(err => {
+        console.log("error connecting to db" + err);
+    });
+
+const PORT = process.env.PORT || 1000;
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
