c409c9fa5ba10439680e1c6ee4c8be400f804962
לילך אברהם <lilachavr123@gmail.com>
2021-02-05 17:43:13 +0200

    create client side with react tsc

---
+++ client_web/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { SignIn, SignUp } from './components/Login';
+
+function App() {
+
+  return (
+    <div className="App">
+<SignIn/>
+<SignUp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client_web/src/components/Login/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SignIn() {
+    return (
+        <div>
+            SignIn
+        </div>
+    )
+}
+
+function SignUp() {
+    return (
+        <div>
+            SignUp
+        </div>
+    )
+}
+export { SignIn, SignUp }
